5a32d343dc35296ea51bdc6305c51b25dc66693b
Waleed Gamal <Waleed.gamal1000@gmail.com>
2019-05-11 23:17:56 +0200

    setup reactotran to work with expo

--- config/Reactotron.js
+++ config/Reactotron.js
@@ -1,8 +1,13 @@
 /* eslint-disable */
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
+import url from "url"
+import { NativeModules } from "react-native";
 
-export default Reactotron.configure({ name: 'mossad' }) // controls connection & communication settings
+
+const { hostname } = url.parse(NativeModules.SourceCode.scriptURL)
+
+export default Reactotron.configure({ name: 'Mossad',  host: hostname }) // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
   .use(reactotronRedux()) // redux plugin
   .connect(); // let's connect!
